308ae93fbb9ca4884d3844db6c1347b21ed03c16
Havi S <taxes@taxes.moe>
2018-02-24 16:39:34 -0600

    Complex card component

--- routes.js
+++ routes.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { } );
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    'card_example_links': [
+      { 'url': '#', 'text': 'This is a link.' },
+      { 'url': '#', 'text': 'This is also a link.' }
+    ],
+    'card_example_links_2': [
+      { 'url': '#', 'text': 'This is a link.' },
+      { 'url': '#', 'text': 'This is also a link.' },
+      { 'url': 'https://www.google.com', 'text': 'This link opens in a new tab/window.', newTab: true }
+    ]
+  });
 });
 
 module.exports = router;
