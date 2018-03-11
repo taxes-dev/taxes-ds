43de67605edb6e27af52ac4ca4aef2484d207bc3
Havi S <taxes@taxes.moe>
2018-03-11 10:21:50 -0500

    At some point I forgot my standard was two underscores

--- scripts/components/tabs.js
+++ scripts/components/tabs.js
@@ -1,12 +1,12 @@
 (function(window, $) {
 
     function tabs(selector) {
-        $(selector).find('.' + TAXESDS_CSS_NAMESPACE + '--tabs_headers_header').on('click', function(evt) {
+        $(selector).find('.' + TAXESDS_CSS_NAMESPACE + '--tabs__headers__header').on('click', function(evt) {
             let target = $(evt.target);
             let tab_id = target.attr('data-target');
-            target.siblings('.' + TAXESDS_CSS_NAMESPACE + '--tabs_headers_header').removeClass('-active');
+            target.siblings('.' + TAXESDS_CSS_NAMESPACE + '--tabs__headers__header').removeClass('-active');
             target.addClass('-active');
-            target.closest('.' + TAXESDS_CSS_NAMESPACE + '--tabs').find('.' + TAXESDS_CSS_NAMESPACE + '--tabs_tabs_tab').each(function() {
+            target.closest('.' + TAXESDS_CSS_NAMESPACE + '--tabs').find('.' + TAXESDS_CSS_NAMESPACE + '--tabs__tabs__tab').each(function() {
                 let current = $(this);
                 if (current.attr('id') === tab_id) {
                     current.addClass('-active');
