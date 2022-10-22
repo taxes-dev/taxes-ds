5d835debfd03ea474eb9731faf2e645fb2e587a1
Havi S <havi@taxes.dev>
2022-10-22 10:10:05 -0400

    Upgrade dependencies, fix a few build steps

--- sass-compile.sh
+++ sass-compile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+mkdir -p public/css
+mkdir -p public/scripts
 yarn run node-sass --include-path ./sass sass/local.scss >public/css/local.css
 yarn run node-sass --include-path ./sass sass/taxes-ds.scss >public/css/taxes-ds.css
